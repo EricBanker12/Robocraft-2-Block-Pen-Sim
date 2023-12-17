0bfe50fb0e98a936a7d41c461305b0e4fbe36779
Eric Banker <EricBanker12@gmail.com>
2023-12-17 16:28:45 -0600

    update weapon stats

--- BlockPenSimWPF/Data/BlockData.cs
+++ BlockPenSimWPF/Data/BlockData.cs
@@ -13,14 +13,14 @@ namespace BlockPenSimWPF.Data
 
         public static Dictionary<string, Weapon> DefaultWeapons = new Dictionary<string, Weapon>()
         {
-            {"LaserBlaster", new Weapon { name = "Laser Blaster", cpu = 20, damage = 24.0, pellets = 1.0, radius = 0.3, energy = 2000.0, cooldown = 0.6, impulse = 2750, splashShape = SplashShape.None  } },
-            {"PlasmaCannonMax", new Weapon { name = "Plasma Cannon (37.5m)", cpu = 60, damage = 600.0, pellets = 9.0, radius = 25.0, energy = 1000.0, cooldown = 2.5, impulse = 2750, splashShape = SplashShape.Cone } },
-            {"PlasmaCannon", new Weapon { name = "Plasma Cannon (78.75m)", cpu = 60, damage = 340.0, pellets = 9.0, radius = 25.0, energy = 1000.0, cooldown = 2.5, impulse = 2750, splashShape = SplashShape.Cone } },
-            {"PlasmaCannonMin", new Weapon { name = "Plasma Cannon (120m)", cpu = 60, damage = 80.0, pellets = 9.0, radius = 25.0, energy = 1000.0, cooldown = 2.5, impulse = 2750, splashShape = SplashShape.Cone } },
+            {"LaserBlaster", new Weapon { name = "Laser Blaster", cpu = 20, damage = 22.0, pellets = 1.0, radius = 0.3, energy = 1500.0, cooldown = 0.6, impulse = 2750, splashShape = SplashShape.None  } },
+            {"PlasmaCannonMax", new Weapon { name = "Plasma Cannon (37.5m)", cpu = 60, damage = 850.0, pellets = 9.0, radius = 25.0, energy = 3000.0, cooldown = 2.5, impulse = 2750, splashShape = SplashShape.Cone } },
+            {"PlasmaCannon", new Weapon { name = "Plasma Cannon (78.75m)", cpu = 60, damage = 465.0, pellets = 9.0, radius = 25.0, energy = 3000.0, cooldown = 2.5, impulse = 2750, splashShape = SplashShape.Cone } },
+            {"PlasmaCannonMin", new Weapon { name = "Plasma Cannon (120m)", cpu = 60, damage = 80.0, pellets = 9.0, radius = 25.0, energy = 3000.0, cooldown = 2.5, impulse = 2750, splashShape = SplashShape.Cone } },
             {"ArcDischarger", new Weapon{ name = "Arc Discharger", cpu = 40, damage = 15000.0, pellets = 11.0, radius = 30.0, energy = 120000.0, cooldown = 6.0, impulse = 5000, splashShape = SplashShape.Cone  } },
             {"RailGunMin", new Weapon      { name = "Rail Gun (25m)", cpu = 120, damage = 150.0, pellets = 1.0, radius = 0.3, energy = 200.0, cooldown = 3.5, impulse = 2750, splashShape = SplashShape.Cylinder  } },
-            {"RailGun", new Weapon      { name = "Rail Gun (47.5m)", cpu = 120, damage = 480.0, pellets = 12.0, radius = 1.5, energy = 2350.0, cooldown = 3.5, impulse = 2750, splashShape = SplashShape.Cylinder  } },
-            {"RailGunMax", new Weapon      { name = "Rail Gun (70m)", cpu = 120, damage = 810.0, pellets = 12.0, radius = 3.0, energy = 4500.0, cooldown = 3.5, impulse = 2750, splashShape = SplashShape.Cylinder  } },
+            {"RailGun", new Weapon      { name = "Rail Gun (47.5m)", cpu = 120, damage = 480.0, pellets = 12.0, radius = 2.5, energy = 2700.0, cooldown = 3.5, impulse = 2750, splashShape = SplashShape.Cylinder  } },
+            {"RailGunMax", new Weapon      { name = "Rail Gun (70m)", cpu = 120, damage = 810.0, pellets = 12.0, radius = 5.0, energy = 5200.0, cooldown = 3.5, impulse = 2750, splashShape = SplashShape.Cylinder  } },
         };
 
         public static Dictionary<string, Material> DefaultMaterials = new Dictionary<string, Material>()
